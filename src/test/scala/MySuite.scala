6ad866a856c8c2a5e4457b2cb5dc84bad5ec6f11
Muqiu Han <muqiu-han@outlook.com>
2023-12-11 21:19:44 +0800

    [Update]: README

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,9 +1,7 @@
+import com.muqiuhan.onopo.*
+
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
-  }
+  test("example test that succeeds") {}
 }
